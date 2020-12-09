4c9c442799f212b09422718eb3e0be2223543906
Bas Tuijnman <bas.tuijnman@24i.com>
2020-12-09 14:24:27 +0100

    Add comments

--- 2020/9-12/src/main.rs
+++ 2020/9-12/src/main.rs
@@ -18,6 +18,8 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
 
     let list: Vec<i64> = reader.lines().map(|line| line.unwrap().parse::<i64>().unwrap()).collect();
+
+    // Preamble numbers should not be used as values.
     let values = list.clone().split_off(preamble);
     
     // Find the invalid number
@@ -32,7 +34,11 @@ fn main() -> io::Result<()> {
     match find_contiguous_set(list, *invalid) {
         None => println!("Did not find set for invalid entry, BAD OUTPUT"),
         Some(mut set) => {
+
+            // Sort the set high to low
             set.sort();
+
+            // Encryption weakness = (lowest + highest)
             println!("Encryption weakness found: {}", set.first().unwrap() + set.last().unwrap())
         }
     }
@@ -40,6 +46,10 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+/**
+ * Finds a (continuous) list of numbers in the list that together
+ * sum to the needle.
+ */
 fn find_contiguous_set(list: Vec<i64>, needle: i64) -> Option<Vec<i64>> {
     for (i, _item) in list.iter().enumerate() {
         let mut acc: Vec<i64> = vec![];
