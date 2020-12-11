0acd69fd16a45a16e20057a0da01ecdef683ca01
Bas Tuijnman <bas.tuijnman@24i.com>
2020-12-11 11:58:47 +0100

    "Optimize" some things for part 2

--- 2020/11-12/src/main.rs
+++ 2020/11-12/src/main.rs
@@ -17,22 +17,24 @@ fn main() -> io::Result<()> {
     let file = File::open(matches.value_of("INPUT").unwrap())?;
     let reader = BufReader::new(file);
 
+    let mode = matches.value_of("mode").unwrap_or("");
+    let empties_at = if mode == "visible" { 5 } else { 4 };
+
     let list: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
     let mut map: Vec<Vec<char>> = list.iter().map(|row| row.chars().collect()).collect();
 
     loop {
 
         let mut new_map: Vec<Vec<char>> = map.clone();
-
         for row in 0..map.len() {
             for col in 0..map[row].len() {
 
                 let seat = map[row][col];
-                let adjacent = get_surrounding_seats(&map, row, col);
+                let adjacent = if mode == "visible" { get_visible_seats(&map, row, col) } else { get_surrounding_seats(&map, row, col) };
 
                 if seat == 'L' && adjacent.matches('#').count() == 0 {
                     new_map[row][col] = '#';
-                } else if seat == '#' && adjacent.matches('#').count() >= 4 {
+                } else if seat == '#' && adjacent.matches('#').count() >= empties_at {
                     new_map[row][col] = 'L';
                 } else {
                     new_map[row][col] = seat;
@@ -65,7 +67,37 @@ fn get_surrounding_seats(map:&Vec<Vec<char>>, row: usize, col: usize) -> String 
         .flatten()
         .collect();
 
-
     // Remove occurance of actual wanted seat
     adjacent.replacen(map[row][col], "", 1)
+}
+
+fn get_visible_seats(map:&Vec<Vec<char>>, row: usize, col: usize) -> String {
+    let directions: Vec<(i32, i32)> = vec![ (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1) ];
+    let y_max = map.len() as i32 - 1;
+    let x_max = map[0].len() as i32 - 1;
+    let mut adjacent = String::new();
+
+    for direction in directions {
+        let (y, x) = direction;
+        let mut current_y = (row as i32) + y;
+        let mut current_x = (col as i32) + x;
+
+        if current_x < 0 || current_y < 0 || current_x > x_max || current_y > y_max {
+            continue;
+        }
+
+        let mut c = map[current_y as usize][current_x as usize];
+        while c != 'L' && c != '#' {
+            current_y = current_y + y;
+            current_x = current_x + x;
+
+            if current_x < 0 || current_y < 0 || current_x > x_max || current_y > y_max {
+                break;
+            }
+            c = map[current_y as usize][current_x as usize];
+        }
+
+        adjacent.push(c);
+    }
+    adjacent
 }
