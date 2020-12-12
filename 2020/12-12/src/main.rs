b701b45975897c5dff8f103cf13ebf8bef370759
Bas Tuijnman <bas.tuijnman@24i.com>
2020-12-12 22:48:00 +0100

    Condensed code

--- 2020/12-12/src/main.rs
+++ 2020/12-12/src/main.rs
@@ -19,7 +19,6 @@ struct Ship {
 }
 
 impl Ship {
-
     fn new() -> Ship {
         Ship {
             direction: "E".to_string(),
@@ -32,8 +31,7 @@ impl Ship {
         match command {
             "F" | "N" | "E" | "S" | "W" => {
                 let direction = if command == "F" { &self.direction } else { command };
-                let count = self.position_tracker.entry(direction.to_string()).or_insert(0);
-                *count += value;
+                *self.position_tracker.entry(direction.to_string()).or_insert(0) += value;
             },
             "R" | "L" => {
                 let mut directions = vec!["N", "E", "S", "W"];
@@ -47,34 +45,11 @@ impl Ship {
     fn process_for_waypoint(&mut self, command: &str, value: i32) {
         match command {
             "F" => {
-                let north = self.waypoint_tracker[0];
-                let east = self.waypoint_tracker[1];
-                let south = self.waypoint_tracker[2];
-                let west = self.waypoint_tracker[3];
+                let north_or_south = self.waypoint_tracker[0] - self.waypoint_tracker[2];
+                *self.position_tracker.entry(if north_or_south > 0 { "N".to_string() } else { "S".to_string() }).or_insert(0) += value * i32::abs(north_or_south);
 
-                // Can move north
-                if north - south > 0 {
-                    let count = self.position_tracker.entry("N".to_string()).or_insert(0);
-                    *count += value * (north - south);
-                }
-
-                // Can move east
-                if east - west > 0 {
-                    let count = self.position_tracker.entry("E".to_string()).or_insert(0);
-                    *count += value * (east - west);
-                }
-
-                // Can move south
-                if south - north > 0 {
-                    let count = self.position_tracker.entry("S".to_string()).or_insert(0);
-                    *count += value * (south - north);
-                }
-
-                // Can move west
-                if west - east > 0 {
-                    let count = self.position_tracker.entry("W".to_string()).or_insert(0);
-                    *count += value * (west - east);
-                }
+                let east_or_west = self.waypoint_tracker[1] - self.waypoint_tracker[3];
+                *self.position_tracker.entry(if east_or_west > 0 { "E".to_string() } else { "W".to_string() }).or_insert(0) += value * i32::abs(east_or_west);
             },
             "N" | "E" | "S" | "W" => {
                 let directions = vec!["N", "E", "S", "W"];
@@ -93,13 +68,8 @@ impl Ship {
     }
 
     fn get_manhattan_distance(self) -> i32 {
-        let north = self.position_tracker.get("N").unwrap_or(&0);
-        let south = self.position_tracker.get("S").unwrap_or(&0);
-        let east = self.position_tracker.get("E").unwrap_or(&0);
-        let west = self.position_tracker.get("W").unwrap_or(&0);
-        i32::abs(east - west) + i32::abs(north - south)
+        i32::abs(self.position_tracker.get("E").unwrap_or(&0) - self.position_tracker.get("W").unwrap_or(&0)) + i32::abs(self.position_tracker.get("N").unwrap_or(&0) - self.position_tracker.get("S").unwrap_or(&0))
     }
-
 }
 
 fn main() -> io::Result<()> {
@@ -121,12 +91,14 @@ fn main() -> io::Result<()> {
         (captures.get(1).unwrap().as_str(), captures.get(2).unwrap().as_str().parse::<i32>().unwrap())
     }).collect();
 
+    // Get manhattan distance for processing regular instructions
     let mut ship = Ship::new();
     for (command, value) in instructions.iter() {
         ship.process(command, *value);
     }
     println!("Part 1: {}", ship.get_manhattan_distance());
 
+    // Get manhattan distance for processing instructions with waypoint logic
     let mut ship = Ship::new();
     for (command, value) in instructions.iter() {
         ship.process_for_waypoint(command, *value);
