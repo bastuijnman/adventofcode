f5552d0a29fc95224b5538f184d0eb3aa153c25f
Bas Tuijnman <bas.tuijnman@24i.com>
2021-12-05 23:51:20 +0100

    Change mechanism to buffer structure & read from virtual table

--- 2021/03-12/answer.c
+++ 2021/03-12/answer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 // Definition of the number of bits per line present in the diagnostics report
 const int NUMBER_OF_BITS = 12;
@@ -9,49 +10,46 @@ int main () {
 
     FILE *f = fopen("input.txt", "r");
 
-    int gamma_counter[NUMBER_OF_BITS] = {0};
-    int char_counter = 0; // Chars start at 0 for 0-based index
-    int line_counter = 1; // We can start lines at 1 assuming we're reading from lines at all
+    int lines = 1;
+    while (EOF != (fscanf(f, "%*[^\n]"), fscanf(f,"%*c")))
+        lines++;
 
     /*
-     * I too like to live dangerously 
+     * Ineffecient AF :(
      */
-    while (1) {
+    int index = 0;
+    int buffer[NUMBER_OF_BITS * lines];
 
-        // Break the loop once the end of the file is reached
-        if (feof(f)) {
-            break;
-        }
-
+    rewind(f);
+    while (!feof(f)) {
         int c = fgetc(f);
 
-        // Increase line counter when newline is reached
-        if (c == 10) { 
-            line_counter++;
-
-            // No need for processing for newlines
-            continue; 
+        // Don't include newlines and EOF chars
+        if (c != 10 && c != EOF) {
+            buffer[index] = c;
+            index++;
         }
+    }
 
-        // Increase count in case the bit is set to 1
-        if (c == 49) {
-            gamma_counter[char_counter % NUMBER_OF_BITS] += 1;
+    int gamma_counter[NUMBER_OF_BITS] = {0};
+    for (int i = 0; i < lines; i++) {
+        for (int j = 0; j < NUMBER_OF_BITS; j++) {
+            gamma_counter[((i * NUMBER_OF_BITS) + j) % NUMBER_OF_BITS] += buffer[(i * NUMBER_OF_BITS) + j] == 49;
         }
-
-        // Increase actual number of chars processed
-        char_counter++;
     }
 
     // Calculate gamma
     int gamma = 0;
     for (int i = 0; i < NUMBER_OF_BITS; i++) {
-        gamma += (gamma_counter[i] >= (line_counter / 2) ? 1 : 0) * pow(2, (NUMBER_OF_BITS - 1) - i);
+        gamma += (gamma_counter[i] >= (lines / 2) ? 1 : 0) * pow(2, (NUMBER_OF_BITS - 1) - i);
     }
 
     // epsilon = XOR with mask consisting of number of bits per line (flips all bits)
     int epsilon = gamma ^ ((1 << NUMBER_OF_BITS) - 1);
-
     printf("Answer 1: %d\n", gamma * epsilon);
 
+    // TODO: answer 2
+
+    fclose(f);
     return 0;
 }
