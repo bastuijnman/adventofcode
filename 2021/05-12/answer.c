b59e1f6f76f5c8dc3aeffc7fc84bc36202e2f958
Bas Tuijnman <bas.tuijnman@24i.com>
2021-12-09 15:03:09 +0100

    day 5 part 2

--- 2021/05-12/answer.c
+++ 2021/05-12/answer.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int min (int a, int b) { return a < b ? a : b; }
 int max (int a, int b) { return a > b ? a : b;}
+int clamp (int a, int min, int max) { if (a < min) return min; if (a > max) return max; return a; }
+
+bool is_straight(int x1, int y1, int x2, int y2) { return x1 == x2 || y1 == y2; }
+bool is_45deg(int x1, int y1, int x2, int y2) {return abs(x1 - x2) == abs(y1 - y2);}
 
 int calculate_overlaps (int mode) {
     FILE *f = fopen("input.txt", "r");
@@ -35,17 +40,23 @@ int calculate_overlaps (int mode) {
     while (fscanf(f, "%d,%d -> %d,%d", &x1, &y1, &x2, &y2) == 4) {
         
         // Skip any non horizontal/vertical lines in hor/ver only mode
-        if (mode == 1 && x1 != x2 && y1 != y2) {
+        if (mode == 1 && !is_straight(x1, y1, x2, y2)) {
+            continue;
+        }
+        if (mode == 2 && !is_straight(x1, y1, x2, y2) && !is_45deg(x1, y1, x2, y2)) {
             continue;
         }
 
-        // Follow lines
-        for (int y = min(y1, y2); y <= max(y1, y2); y++) {
-            for (int x = min(x1, x2); x <= max(x1, x2); x++) {
-                grid[y][x]++;
-            }
-        }
-        
+        // Set direction
+        int ix = clamp(x2 - x1, -1, 1), iy = clamp(y2 - y1, -1, 1);
+
+        // First step out of while
+        grid[y1][x1]++;
+        while (x1 != x2 || y1 != y2) {
+            x1 += ix;
+            y1 += iy;
+            grid[y1][x1]++;
+        }    
     }
 
     // Calculate overlaps
@@ -54,11 +65,14 @@ int calculate_overlaps (int mode) {
         if (grid[i / maxX][i % maxX] > 1) overlaps++;
     }
 
+    fclose(f);
+
     return overlaps;
 }
 
 int main () {
     printf("Answer 1: %d\n", calculate_overlaps(1));
+    printf("Answer 2: %d\n", calculate_overlaps(2));
 
     return 0;
 }
