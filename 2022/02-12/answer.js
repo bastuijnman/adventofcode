f20b21211c19a48ee18564fcd8527a073b7d1f0d
Bas Tuijnman <bas.tuijnman@24i.com>
2022-12-02 22:49:13 +0100

    Day 2, answer 2

--- 2022/02-12/answer.js
+++ 2022/02-12/answer.js
@@ -1,23 +1,25 @@
 import { readFile } from 'fs/promises';
 
-const scoreMatrix = {
-    'A': ['X', 'Y'],
-    'B': ['Y', 'Z'],
-    'C': ['Z', 'X']
-};
-
+const scoreMatrix = { 'A': 'ZXY', 'B': 'XYZ', 'C': 'YZX' };
 const score = game => 'XYZ'.indexOf(game[2])+1;
-const outcome = (game) => (scoreMatrix[game[0]].indexOf(game[2]) + 1) * 3;
+const outcome = game => scoreMatrix[game[0]].indexOf(game[2]) * 3;
+const sanitize = game => `${game[0]} ${scoreMatrix[game[0]]['XYZ'.indexOf(game[2])]}`;
+const cummulative = (prev, cur) => prev + cur;
 
 const answer = async () => {
-    const input = await readFile('input.txt');
-
-    const totals = input
+    const input = (await readFile('input.txt'))
         .toString()
-        .split('\n')
+        .split('\n');
+    
+    const totals = input
         .map(game => outcome(game) + score(game));
 
-    console.log(`Answer 1: ${totals.reduce((prev, cur) => prev + cur)}`);
+    const totalsFixed = input
+        .map(game => sanitize(game))
+        .map(game => outcome(game) + score(game));
+
+    console.log(`Answer 1: ${totals.reduce(cummulative)}`);
+    console.log(`Answer 1: ${totalsFixed.reduce(cummulative)}`);
 }
 
 answer();
