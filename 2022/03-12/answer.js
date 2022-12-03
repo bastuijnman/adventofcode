6c8e58433d6a7ba0ad7ebf374e4fff5f7d55817c
Bas Tuijnman <bas.tuijnman@24i.com>
2022-12-03 20:27:15 +0100

    Day 3, answer 2

--- 2022/03-12/answer.js
+++ 2022/03-12/answer.js
@@ -3,8 +3,7 @@ import { readFile } from 'fs/promises';
 const priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const split = r => [r.substring(0, r.length / 2), r.substring(r.length / 2)];
-const compare = r => r[0].split('').filter(c => r[1].includes(c)).concat(r[1].split('').filter(c => r[0].includes(c)));
-const unique = r => r.filter((c, i) => r.indexOf(c) === i);
+const compare = r => r[0].split('').filter(c => r.filter(s => s.includes(c)).length === r.length);
 const calculatePriority = r => priorities.indexOf(r[0]) + 1;
 const sum = (cur, prev) => prev + cur;
 
@@ -16,11 +15,20 @@ const answer = async () => {
     const prioritySum = input
         .map(split)
         .map(compare)
-        .map(unique)
+        .map(calculatePriority)
+        .reduce(sum, 0);
+
+    const groupInput = [].concat(input);
+    const groups = [];
+    while (groupInput.length) { groups.push(groupInput.splice(0, 3)); };
+
+    const groupPrioritySum = groups
+        .map(compare)
         .map(calculatePriority)
         .reduce(sum, 0);
 
     console.log(`Answer 1: ${prioritySum}`);
+    console.log(`Answer 2: ${groupPrioritySum}`);
 };
 
 answer();
