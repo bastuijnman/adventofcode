b78f716d1877a24b7dfffe1c62bdffe2482d2c0c
Bas Tuijnman <bas.tuijnman@mobian.global>
2023-12-01 21:24:34 +0100

    Compress code for day 1 a bit

--- 2023/01-12/src/main.rs
+++ 2023/01-12/src/main.rs
@@ -1,26 +1,13 @@
-use std::{env, fs::read_to_string, collections::HashMap};
+use std::{env, fs::read_to_string, collections::HashMap, cmp};
 
 fn list_numbers(line: &String) -> Vec<char> {
-    line
-        .chars()
-        .filter(|c: &char| c.is_digit(10))
-        .collect()
+    line.chars().filter(|c: &char| c.is_digit(10)).collect()
 }
 
 fn list_numbers_including_numbers_as_words(line: &String) -> Vec<char> {
 
     // List out a map of the number words and their corresponding integer as a char
-    let map: HashMap<&str, char> = HashMap::from([
-        ("one", '1'),
-        ("two", '2'),
-        ("three", '3'),
-        ("four", '4'),
-        ("five", '5'),
-        ("six", '6'),
-        ("seven", '7'),
-        ("eight", '8'),
-        ("nine", '9')
-    ]);
+    let map: HashMap<&str, char> = HashMap::from([("one", '1'),("two", '2'),("three", '3'),("four", '4'),("five", '5'),("six", '6'),("seven", '7'),("eight", '8'),("nine", '9')]);
 
     line
         .chars()
@@ -32,33 +19,32 @@ fn list_numbers_including_numbers_as_words(line: &String) -> Vec<char> {
                 c
             } else {
 
-                // Loop over all hashmap keys and try to find one of them starting from the current char.
-                let found = map
-                    .keys()
-                    .find(|key| {
+                // Calculate max string slice size 
+                let end = cmp::min(idx + 5, line.len());
 
-                        // If we get the substring (not the case when going over max string length) then check if 
-                        // it equals the key we're trying to find. If it equals it mark the key as found. 
-                        match line.get(idx..idx+key.len()) {
-                            Some(_key) => _key == **key,
-                            None => false
-                        }
-                    });
+                // Grab string slice
+                match line.get(idx..end) {
 
-                // Check if we found a key, if we did we can return the value from the hash map, otherwise
-                // we can just return a nonsensical char
-                match found {
-                    Some(key) => *map.get(key).unwrap(),
+                    // Check if the slice starts with one of the mapped keys
+                    Some(_key) => match map.keys().find(|k| _key.starts_with(**k)) {
+
+                        // If found grab the char value from the hashmap
+                        Some(_found) => *map.get(_found).unwrap(),
+
+                        // Nothing found, arbitrary char
+                        None => 'N'
+                    }, 
+
+                    // Nothing found, arbitrary char
                     None => 'N'
                 }
-
             }
         })
         .filter(|c| c.is_digit(10)) // Filter out any non digit chars
         .collect() // Collect into vector
 }
 
-fn format_line_number(line: Vec<char>) -> i32 {
+fn format_and_convert_line_number(line: Vec<char>) -> i32 {
     // Concat chars and parse into integer
     format!("{}{}", line.first().unwrap(), line.last().unwrap()).parse::<i32>().unwrap()
 }
@@ -72,18 +58,10 @@ fn main() {
     let lines: Vec<String> = contents.lines().map(|l| l.to_owned()).collect::<Vec<String>>();
 
     // Map out all known integer numbers into a result vec, then fold into the answer
-    let total_part_one: i32 = lines
-        .iter()
-        .map(list_numbers)
-        .map(format_line_number)
-        .fold(0, |acc, next| acc + next);
+    let total_part_one: i32 = lines.iter().map(list_numbers).map(format_and_convert_line_number).sum();
 
     // Map out all known integer numbers & word numbers into a result vec, then fold into the answer
-    let total_part_two: i32 = lines
-        .iter()
-        .map(list_numbers_including_numbers_as_words)
-        .map(format_line_number)
-        .fold(0, |acc, next| acc + next);
+    let total_part_two: i32 = lines.iter().map(list_numbers_including_numbers_as_words).map(format_and_convert_line_number).sum();
 
     println!("Answer part one: {}", total_part_one);
     println!("Answer part two: {}", total_part_two);
